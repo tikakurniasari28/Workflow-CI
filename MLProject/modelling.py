2feb8abf604b747cdaf96e15426ea76cb7567459
tikakurniasari28 <ntikakur@gmail.com>
2025-12-26 01:47:39 +0800

    test6

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -28,13 +28,16 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+with mlflow.start_run():
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+    y_pred = model.predict(X_test)
 
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
 
-print("MSE:", mse)
-print("R2:", r2)
+    print("MSE:", mse)
+    print("R2:", r2)
+
+    mlflow.sklearn.log_model(model, artifact_path="model")
