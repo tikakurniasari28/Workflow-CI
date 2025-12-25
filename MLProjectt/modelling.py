7661434ed5ff2787278cbcdeb39c728547f3ca36
tikakurniasari28 <ntikakur@gmail.com>
2025-12-26 00:08:21 +0800

    commit satu

--- MLProjectt/modelling.py
+++ MLProjectt/modelling.py
@@ -1,17 +1,19 @@
 import mlflow
 import mlflow.sklearn
-
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("CI-Retraining-Experiment")
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from pathlib import Path
 
+# === Setup MLflow tracking local ===
+Path("mlruns").mkdir(exist_ok=True)
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("CI-Retraining-Experiment")
+
 mlflow.sklearn.autolog()
 
+# === Load Data ===
 BASE_DIR = Path(__file__).resolve().parent
 data_path = BASE_DIR / "day_wise_processed.csv"
 
@@ -20,14 +22,16 @@ df = pd.read_csv(data_path)
 X = df.iloc[:, :-1]
 y = df.iloc[:, -1]
 
+# === Split Data ===
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
+# === Train Model ===
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+# === Evaluate ===
 y_pred = model.predict(X_test)
 
 mse = mean_squared_error(y_test, y_pred)
