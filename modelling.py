4334c935a45d489cef1ada7161b0867d3a5df5ac
tikakurniasari28 <ntikakur@gmail.com>
2025-12-30 08:39:33 +0800

    test1

--- modelling.py
+++ modelling.py
@@ -9,15 +9,13 @@ import mlflow.sklearn
 BASE_DIR = Path(__file__).resolve().parent
 
 def main():
-
     mlflow.set_tracking_uri(f"file://{BASE_DIR}/mlruns")
-    mlflow.set_experiment("default")
+    mlflow.set_experiment("ci-experiment")
 
-    data_path = BASE_DIR / "day_wise_processed.csv"
-    df = pd.read_csv(data_path)
+    df = pd.read_csv(BASE_DIR / "day_wise_processed.csv")
 
-    y = df["Confirmed"]
     X = df.drop(columns=["Confirmed"])
+    y = df["Confirmed"]
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
@@ -26,22 +24,14 @@ def main():
     model = LinearRegression()
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
+    preds = model.predict(X_test)
 
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
+    mlflow.log_metric("mse", mean_squared_error(y_test, preds))
+    mlflow.log_metric("r2", r2_score(y_test, preds))
 
-    mlflow.log_metric("mse", mse)
-    mlflow.log_metric("r2", r2)
+    mlflow.sklearn.log_model(model, artifact_path="model")
 
-    mlflow.sklearn.log_model(
-        model,
-        artifact_path="model"
-    )
-
-    print("Training selesai")
-    print("MSE:", mse)
-    print("R2:", r2)
+    print("Training selesai via CI")
 
 if __name__ == "__main__":
     main()
